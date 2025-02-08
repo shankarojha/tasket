402cc440334cd7ac5cc36ad356ba5bbe367edbca
Shankar Ojha <shankarojha34@gmail.com>
2025-02-08 18:26:13 +0530

    added structure, added models

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const MONGO_URI = process.env.MONGO_URI as string //importing it as a string
+
+export const connectDB = async () => {
+  try {
+    if (mongoose.connection.readyState >= 1) return //check for the ready state
+    await mongoose.connect(MONGO_URI) //connecting to mongo uri
+    console.log("Connected to db");
+  } catch (error) {
+    console.error("DB connection failed:", error);
+  }
+};
---
+++ src/models/task.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: {type:String, required:true},
+  status: { type: String, enum: ["assigned", "in-progress", "completed"], default: "assigned" },
+  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  priority: { type: String, enum: ["low", "medium", "high"], default: "medium" },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Task", taskSchema)
