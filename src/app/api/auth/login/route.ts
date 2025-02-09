cdc0a29fa6fefb48077c405e90620cf49831676a
Shankar Ojha <shankarojha34@gmail.com>
2025-02-09 23:43:10 +0530

    added FE structure and middleware for route guard

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,9 +1,72 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
-import User from "@models/user"
+import User from "@models/user";
 import { connectDB } from "@/lib/db";
+import { GlobalResponse } from "@/types/globalResponse";
+import { z } from "zod";
+import { generateToken } from "@/lib/token";
+import { TUser } from "@/types/mongodbtypes";
 
-export async function POST(req:Request) {
-    const {email, name, password, role} =  await req.json();
-    return NextResponse.json({email, name, password, role})
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export async function POST(req: Request) {
+  try {
+    await connectDB();
+    const body = await req.json();
+    const parsed = loginSchema.safeParse(body);
+
+    if (!parsed.success) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid input",
+          error: parsed.error.errors,
+          data: null,
+        },
+        { status: 400 }
+      );
+    }
+    const { email, password } = parsed.data; //extracting from body
+    const user: any= await User.findOne({ email }).lean();
+    if (!user) {
+      const response: GlobalResponse = {
+        success: false,
+        message: "User not found",
+        error: "User not found",
+        data: null,
+      };
+      return NextResponse.json(response, { status: 404 });
+    }
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) {
+      const response: GlobalResponse = {
+        success: false,
+        message: "Incorrect email or password",
+        error: "Incorrect email or password",
+        data: null,
+      };
+      return NextResponse.json(response, { status: 401 });
+    }
+    const { password: _, __v, ...userWoPassword } = user; // removing password from response
+    const token = generateToken(userWoPassword);
+    const response: GlobalResponse = {
+      success: true,
+      message: "User Loggedin successfully",
+      error: null,
+      data: { userWoPassword, token },
+    };
+    return NextResponse.json(response, { status: 200 });
+  } catch (error: any) {
+    console.log(error);
+    const response: GlobalResponse = {
+      success: false,
+      message: "User registration failed",
+      error: error.message,
+      data: null,
+    };
+    return NextResponse.json(response, { status: 501 });
+  }
 }
--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,12 +1,29 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import User from "@models/user";
-import { connectDB } from "@/lib/db";
+import { connectDB } from "@/lib/db"
 import { GlobalResponse } from "@/types/globalResponse";
+import {z} from "zod"
+
+const signupSchema = z.object({
+  email:z.string().email(), 
+  name: z.string(), 
+  password:z.string(), 
+  role:z.enum(["manager", "user"])
+})
 
 export async function POST(req: Request) {
   try {
-    const { email, name, password, role } = await req.json(); //extracting from body
+    const body = await req.json();
+    const parsed = signupSchema.safeParse(body);
+
+    if (!parsed.success) {
+      return NextResponse.json(
+        { success: false, message: "Invalid input", error: parsed.error.errors, data: null },
+        { status: 400 }
+      );
+    }
+    const { email, name, password, role } = parsed.data; //extracting from body
     await connectDB();
     const hashedPassword: string = await bcrypt.hash(password, 10); //hashing password
     const newUser = new User({ email, name, password: hashedPassword, role });
